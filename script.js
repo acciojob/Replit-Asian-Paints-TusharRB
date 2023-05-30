174dc0e3fc869d9588c89f8e8015ede19c5738bd
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-05-30 11:09:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.getElementById("change_button").addEventListener("click", function() {
+  var blockId = document.getElementById("block_id").value;
+  var colorId = document.getElementById("colour_id").value;
+  
+  var gridItem = document.getElementById(blockId);
+  if (gridItem) {
+    gridItem.style.backgroundColor = colorId;
+  }
+});
+
+document.getElementById("reset_button").addEventListener("click", function() {
+  var gridItems = document.getElementsByClassName("grid-item");
+  for (var i = 0; i < gridItems.length; i++) {
+    gridItems[i].style.backgroundColor = "transparent";
+  }
+});
